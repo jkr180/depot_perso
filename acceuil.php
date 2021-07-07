21bac35b9c994483b6b9146ccd839d9d039b80c9
jkr180 <77280717+jkr180@users.noreply.github.com>
2021-07-07 09:47:52 +0200

    Add files via upload

---
+++ acceuil.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <title>Jarditou</title> 
+</head>
+<body>
+
+      <h1><p align="center">Liste des Produits</p></h1>
+     
+    <table style="width:100%">
+        <table class="table table-bordered">
+        
+        
+           <tr>
+               <tr class="table-active">
+               <td>Photo</td>
+               <td> ID </td>
+               <td>Référence</td>
+               <td>Libellé</td>
+               <td>Prix</td>
+               <td>stock</td>
+               <td>Couleur</td>
+               <td>Ajout</td>                
+               <td>Modif</td>
+               <td>Bloqué</td>
+           </tr>
+           </tr>
+
+           <tr>
+               <tr class="table">
+               <td><img src="7.jpg" width="100" height="100"></td>
+               <td>7 </td>
+               <td>barb001</td>
+               <td><a href="produit_ajout.php" title="detail">Aramis</a></td>
+               <td>110.00€</td>
+               <td>2</td>
+               <td>brun</td>
+               <td>2018-04-18</td>
+               <td></td>
+               </tr>
+           </tr>
+
+
+           <tr>
+               <tr class="table-active">
+               <td><img src="8.jpg" width="100" height="100"></td>
+               <td>8 </td>
+               <td>barb002</td>
+               <td><a href="produit_ajout.php" title="detail">Athos</a></td>
+               <td>249.99€</td>
+               <td>0</td>
+               <td>Noir</td>
+               <td>2016-06-14</td>
+               <td></td>
+               <td></td>
+           </tr>
+           <tr>
+               
+               <tr class="table">
+               <td><img src="11.jpg" width="100" height="100"></td>
+               <td>11</td>
+               <td>barb003</td>
+               <td><a href="produit_ajout.php" title="detail">Clatronic</a></td>
+               <td>135.90€</td>
+               <td>10</td>
+               <td>Chrome</td>
+               <td>2017-10-18</td>
+               <td>2018-08-23 00:00:00</td>
+               <td></td>
+           </tr>
+        </tr>
+
+           <tr>
+              <tr class="table-active">
+              <td><img src="12.jpg" width="100" height="100"></td>
+              <td>12</td> 
+              <td>barb004</td>
+              <td><a href="produit_ajout.php" title="detail">Camping</a></td>
+              <td>88.00€</td>
+              <td>5</td>
+              <td>Noir</td>
+              <td> 2018-08-20</td>
+              <td></td>
+              <td>Oui</td>
+           </tr>
+           </tr>
+
+           <tr>
+               <tr class="table">
+               <td><img src="13.jpg" width="100" height="100"></td>
+               <td>13</td>
+               <td>barb001</td>
+               <td><a href="produit_ajout.php" title="detail">Green</a></td>
+               <td>49.00€</td>
+               <td>25</td>
+               <td>Verte</td>
+               <td>2018-08-01</td>
+               </tr>
+           </tr>
+           <tr>
+            <tr class="table-active">
+            <td><img src="14.jpg" width="100" height="100"></td>
+            <td>14</td>
+            <td>barb002</td> 
+            <td><a href="produit_ajout.php" title="detail">Silver</a></td>
+            <td>88.00€</td>
+            <td>0</td>
+            <td>Argent</td>
+            <td> 2018-08-09</td>
+            <td>2018-08-22 00:00:00</td>          
+            <td>Oui</td>           
+         </tr>
+         </tr>
+         <tr>
+          <tr class="table">
+          <td><img src="15.jpg" width="100" height="100"></td>
+          <td>15</td>
+          <td>barb003</td>
+          <td><a href="produit_ajout.php" title="detail">Yellow</a></td>
+          <td>54.49€</td>
+          <td>3</td>
+          <td>Jaune</td>
+          <td>2018-08-12</td>
+          </tr>
+      </tr>
+         </tbody>
+     </table>
+   <nav class="navbar navbar-expand navbar-dark bg-dark">
+    
+
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="">Mention légale</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="">Horaires</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="">Plan du site</a>
+        </li>
+    </button>
+      </ul>
+    </div>
+  </nav>
+              </div>
+
+                <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+
+  </body>
+    </htlm>
+
+
+  
+   
+
+    
+ 
